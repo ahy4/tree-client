3b5a90867e127f63cc17070e10e5f3f41589b180
ahyahya <mrfool38@gmail.com>
2017-02-13 13:07:46 +0900

    modify tree

--- public/src/components/tweet-list.js
+++ public/src/components/tweet-list.js
@@ -14,14 +14,20 @@ class TweetNode {
     this.childIds.push(other.statusId);
     other.parentId = this.statusId;
   }
-}
+  findAllChildren(tweetNodes) { // thisの子を探す
+    const children = tweetNodes.filter((child) => {
+      return ~this.childIds.indexOf(child.statusId) && child.level >= 2;
+    });
+    if (children.length === 0) return children; // 子がいなくなったら探さない
+    const childrenOfChildren = Array.prototype.concat.apply([], // flatten
+      children.map((c) => c.findAllChildren(tweetNodes))
+    );
+    return children.concat(childrenOfChildren); // 子と探されるだろう子たちをマージして返す
+  }
+  static createTree(tweetNodes) {
+    let tws = tweetNodes.concat();
 
-export default class TweetList extends Component {
-  render() {
-    console.log("tweets:");
-    const tweets = this.props.tweets;
-    // console.log(JSON.stringify(tweets,null,"  "));
-    const tws = tweets.map((tw) => new TweetNode(tw));
+    // とにかく木構造にするため繋げる
     let prev = tws[0];
     tws.forEach((tw, i) => { if (i !== 0) {
       if (tw.parentId !== null) {
@@ -38,18 +44,40 @@ export default class TweetList extends Component {
       prev.addChild(tw);
       prev = tw;
     }});
-    console.log(tws);
 
-    const tweetNodes = tweets.map((tweet, idx) => {
+    // 順番を整理
+    let twsClone = tws.concat();
+    tws.filter((tw) => { // 枝分かれしてるlevel1
+      return tw.childIds.length >= 2 && tw.level == 1;
+    }).map((tw) => {return { // twとその子達をさがす
+      root: tw,
+      children: tw.findAllChildren(tws)
+    }}).forEach(({root, children}, i) => {
+      const rootIdx = twsClone.map(o => o.statusId).indexOf(root.statusId);
+      twsClone.splice(rootIdx+1, 0, children); // 根の次の要素として子を追加
+      twsClone = Array.prototype.concat.apply([], twsClone); // flatten
+      children.forEach((c) => { // 子を消す
+        const childIdx = twsClone.map(o => o.statusId).lastIndexOf(c.statusId);
+        twsClone.splice(childIdx, 1);
+      });
+    });
+
+    return twsClone;
+  }
+}
+
+export default class TweetList extends Component {
+  render() {
+    const tws = TweetNode.createTree(this.props.tweets.map((tw) => new TweetNode(tw)));
+    console.log(tws);
+    const tweetComponents = tws.map((tw, idx) => {
       return (
-        <Tweet userName={tweet.user.name} key={idx}>
-          {idx+": "+tweet.text}
-        </Tweet>
+        <Tweet tweetNode={tw} key={`TweetComponent: ${idx}`} idx={idx} />
       );
     });
     return (
       <div className="tweetList">
-        {tweetNodes}
+        {tweetComponents}
       </div>
     );
   }
--- public/src/components/tweet.js
+++ public/src/components/tweet.js
@@ -1,23 +1,21 @@
 import React, { Component, PropTypes } from 'react';
 
 export default class Tweet extends Component {
-  rawMarkup() {
-    let rawMarkup = marked(this.props.children.toString(), {sanitize: true});
-    return { __html: rawMarkup };
-  }
   render() {
     return (
       <div className="tweet">
         <h2 className="tweetUserName">
-          {this.props.userName}
+          {this.props.tweetNode.content.user.name}
         </h2>
-        <span dangerouslySetInnerHTML={this.rawMarkup()} />
+        <p>
+          {this.props.idx}: {this.props.tweetNode.content.text}
+        </p>
       </div>
     );
   }
 }
 
 Tweet.propTypes = {
-  userName: PropTypes.string.isRequired,
-  children: PropTypes.string.isRequired
+  idx: PropTypes.number.isRequired,
+  tweetNode: PropTypes.any.isRequired
 };
