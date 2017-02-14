b7e07a90fb4ceb9961a9e1e9e0e1a409636396fe
ahyahya <mrfool38@gmail.com>
2017-02-14 18:56:38 +0900

    change env variable setting

--- server-modules/config-wrap.js
+++ server-modules/config-wrap.js
@@ -4,15 +4,17 @@ if (process.env.NODE_ENV === "production") {
   conf = {
     consumer_key: process.env.consumer_key,
     consumer_secret: process.env.consumer_secret,
-    hash_password: process.env.hash_password
+    hash_password: process.env.hash_password,
+    url: process.env.url
   };
+  // console.log(conf);
 } else {
   try {
     conf = require('./config.json');
   } catch (e) {
     throw new Error("Could not read /server-modules/config.json. \n"+
       "Please create it or use [ $ NODE_ENV=production ] or [ $ npm run debug ] and set VARS. \n"+
-      "It needs consumer_key, consumer_secret, hash_password. hash_password is your password.");
+      "It needs consumer_key, consumer_secret, hash_password, url. hash_password is your password.");
   }
 }
 
